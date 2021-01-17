1e762e33dd596064ab717352c9ac2ad16986867b
BingNa Yang <bingnayang@gmail.com>
2021-01-16 20:37:31 -0500

    Initial commit

---
+++ src/main/java/com/threatre/AvatarMovieThreatre/AvatarMovieThreatreApplication.java
@@ -0,0 +1,13 @@
+package com.threatre.AvatarMovieThreatre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvatarMovieThreatreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvatarMovieThreatreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/threatre/AvatarMovieThreatre/AvatarMovieThreatreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threatre.AvatarMovieThreatre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvatarMovieThreatreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
