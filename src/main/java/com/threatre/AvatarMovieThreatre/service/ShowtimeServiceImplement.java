6f0f5eecab782535d665e33a93b1aefec49f82aa
BingNa Yang <bingnayang@gmail.com>
2021-02-08 17:52:03 -0500

    create ShowtimeService interface and implement

---
+++ src/main/java/com/threatre/AvatarMovieThreatre/service/ShowtimeServiceImplement.java
@@ -0,0 +1,13 @@
+package com.threatre.AvatarMovieThreatre.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShowtimeServiceImplement implements ShowtimeService {
+
+	@Autowired
+	public ShowtimeServiceImplement() {
+		
+	}
+}
