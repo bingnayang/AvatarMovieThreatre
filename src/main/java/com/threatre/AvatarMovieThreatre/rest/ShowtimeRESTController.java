9ef2c2790841191930323ba2b30938c581eedc6e
BingNa Yang <bingnayang@gmail.com>
2021-02-08 18:07:26 -0500

    connecting Showtime Controller, Service, and DAO

--- src/main/java/com/threatre/AvatarMovieThreatre/rest/ShowtimeRESTController.java
+++ src/main/java/com/threatre/AvatarMovieThreatre/rest/ShowtimeRESTController.java
@@ -5,14 +5,17 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.threatre.AvatarMovieThreatre.service.ShowtimeService;
+
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ShowtimeRESTController {
-
+	private ShowtimeService showtimeService;
+	
 	@Autowired
-	public ShowtimeRESTController() {
-		
+	public ShowtimeRESTController(ShowtimeService showtimeService) {
+		this.showtimeService = showtimeService;
 	}
 	
 }
--- src/main/java/com/threatre/AvatarMovieThreatre/service/ShowtimeServiceImplement.java
+++ src/main/java/com/threatre/AvatarMovieThreatre/service/ShowtimeServiceImplement.java
@@ -3,11 +3,14 @@ package com.threatre.AvatarMovieThreatre.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.threatre.AvatarMovieThreatre.dao.ShowtimeDAO;
+
 @Service
 public class ShowtimeServiceImplement implements ShowtimeService {
-
+	private ShowtimeDAO showtimeDAO;
+	
 	@Autowired
-	public ShowtimeServiceImplement() {
-		
+	public ShowtimeServiceImplement(ShowtimeDAO showtimeDAO) {
+		this.showtimeDAO = showtimeDAO;
 	}
 }
